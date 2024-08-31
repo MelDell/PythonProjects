b02ad3b9a47c030076b048a199c58b7571572a3c
melchiordelloye <melchiordelloye@gmail.com>
2024-08-31 16:07:20 +0100

    my second commit

--- python_tests_1.py
+++ python_tests_1.py
@@ -3,6 +3,7 @@ import random
 #this function returns a random number between 1 and 100
 def random_number():
    return random.randint(1, 100)
+#
 
 if __name__ == "__main__":
     print(f'random number :{random_number()}')
